68258f7dda3cb05c7b183e0fa1d52b0c3f3ecd22
Edward Tang <bingdian925@gmail.com>
2016-01-28 00:45:02 -0600

    Change the methods of matching  duplicates in DuplicatePipeline

--- twitterscraper/pipelines.py
+++ twitterscraper/pipelines.py
@@ -25,18 +25,14 @@ class DuplicatesPipeline(MongoDBPipeline):
         matching_item = self.collection.find_one(
             {#'session_id': item['session_id'],
              'tweet_id': item['tweet_id'],
+             'keyword': item['keyword'],
              'user_name': item['user_name']}
         )
         if matching_item is not None:
-            Tracer()()
+            # Tracer()()
             raise DropItem(
-                "Duplicate found for %s, %s" %
-                (item['user_name'], item['tweet_id'])
+                "Duplicate found for %s, %s, %s" %
+                (item['user_name'], item['tweet_id'],item['keyword'])
             )
         else:
             return item
-
-
-
-
-
